6741c07d49fe872b746e70cb2052df42fda68bcb
Lillian <59583025+lragudo@users.noreply.github.com>
2020-03-19 12:43:34 -0700

    Filter content structure added
    *poetry moved to a sperate page
    *worked on css on the filter content
    *isotope js added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,30 @@
+// light gallery
 lightGallery(document.getElementById('gallery'), {
     download: false
   });
+
+  // isotope
+const iso = new Isotope( '.filter-container', {
+  itemSelector: '.content'
+});
+
+/* 
+- CLICK BUTTON, FILTER CONTENT
+*/
+
+// cache filters 
+const filters = document.querySelector('.filters');
+
+// click a filter, then filter content-children
+filters.addEventListener( 'click', function( event ) {
+
+  // not `const` because this value changes. use `let`   
+  let filterValue = event.target.getAttribute('data-filter');
+  // check-check
+  console.log(filterValue); 
+
+  // Isotope re-arrange content by filtered value
+  iso.arrange({ filter: filterValue });
+
+});
+
